d621bc9c0437e6663ebf34e7fec4440eaad734ad
Zeuslawyer <zuin.pratap@gmail.com>
2018-01-14 18:29:41 +1100

    add ComponentListener to listen to QOE events

--- exoplayer-codelab-02/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ exoplayer-codelab-02/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -19,12 +19,16 @@ import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.audio.AudioRendererEventListener;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashChunkSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
@@ -37,6 +41,7 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 /**
  * A fullscreen activity to play audio or video streams.
@@ -53,12 +58,17 @@ public class PlayerActivity extends AppCompatActivity {
   private int currentWindow;
   private boolean playWhenReady = true;
 
+  // https://codelabs.developers.google.com/codelabs/exoplayer-intro/#5
+  private ComponentListener componentListener;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_player);
 
     playerView = findViewById(R.id.video_view);
+
+    componentListener = new ComponentListener;
   }
 
   @Override
@@ -102,9 +112,13 @@ public class PlayerActivity extends AppCompatActivity {
       // let the factory create a player instance with default components
       player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
           new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
+
       playerView.setPlayer(player);
       player.setPlayWhenReady(playWhenReady);
       player.seekTo(currentWindow, playbackPosition);
+      player.addListener(componentListener); //// https://codelabs.developers.google.com/codelabs/exoplayer-intro/#5
+      player.addAudioDebugListener((AudioRendererEventListener) componentListener);  // https://codelabs.developers.google.com/codelabs/exoplayer-intro/#5
+      player.addVideoDebugListener((VideoRendererEventListener) componentListener);
     }
     MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_dash)));
     player.prepare(mediaSource, true, false);
@@ -115,6 +129,9 @@ public class PlayerActivity extends AppCompatActivity {
       playbackPosition = player.getCurrentPosition();
       currentWindow = player.getCurrentWindowIndex();
       playWhenReady = player.getPlayWhenReady();
+      player.removeListener(componentListener);
+      player.removeAudioDebugListener((AudioRendererEventListener) componentListener);
+      player.removeVideoDebugListener((VideoRendererEventListener) componentListener);
       player.release();
       player = null;
     }
@@ -138,4 +155,32 @@ public class PlayerActivity extends AppCompatActivity {
         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
   }
 
+  private class ComponentListener extends Player.DefaultEventListener {
+      private static final String TAG = "ComponentListener";
+
+      @Override
+      public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+          String stateString;
+          switch (playbackState) {
+              //case ExoPlayer.STATE_IDLE:   - deprecated
+              case Player.STATE_IDLE:
+                  stateString = "Player.STATE_IDLE      -";
+                  break;
+              case Player.STATE_BUFFERING:
+                  stateString = "Player.STATE_BUFFERING -";
+                  break;
+              case Player.STATE_READY:
+                  stateString = "Player.STATE_READY     -";
+                  break;
+              case Player.STATE_ENDED:
+                  stateString = "Player.STATE_ENDED     -";
+                  break;
+              default:
+                  stateString = "UNKNOWN_STATE             -";
+                  break;
+          }
+          Log.d(TAG, "changed state to " + stateString
+                  + " playWhenReady: " + playWhenReady);
+      }
+  }
 }
